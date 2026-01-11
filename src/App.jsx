18b1655aeb91e6874018329a6c0e3feab786afb1
snehachy12 <sneha.p.chy04@gmail.com>
2026-01-11 19:34:35 +0530

    ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
+import Main from './components/Sidebar/main/Main'
 
 const App = () => {
   return (
     <>
     <Sidebar/>
+    <Main/>
     </>
   )
 }
